902e7c8a434453ef0c0adf46f422e37281d65f3c
R-LIX <davisrankwillix@gmail.com>
2026-03-06 19:49:53 +0700

    new

--- main.java
+++ main.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class main {
-    
-}
+    public static void main(String[] args) {
+        //Fei and Lika
+        //Input and output recall
+        
+        
+        
+        
+        //Hour and Yuth
+        //Password Validation
+ 
+        
+
+
+        //Lix and miy
+        //Points calculator and output
+      
+       
+        }
+    }

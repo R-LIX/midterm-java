421d7143f259c65940239d1af94c4bcaa5123860
R-LIX <davisrankwillix@gmail.com>
2026-03-07 19:17:36 +0700

    testing

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class main {
     public static void main(String[] args) {
         //Fei and Lika
         //Input and output recall
-        
+        //I want to add smth here to test the commit and push function of git
         
         
         

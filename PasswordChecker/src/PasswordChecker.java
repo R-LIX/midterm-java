ab8a820719012890016fd87fc6e21acb934200cf
R-LIX <davisrankwillix@gmail.com>
2026-03-07 20:52:06 +0700

    intro

--- PasswordChecker/src/PasswordChecker.java
+++ PasswordChecker/src/PasswordChecker.java
@@ -1,19 +1,30 @@
 import exception.InvalidPasswordException;
 import model.Password;
-
+import logic.PasswordScore;
 import java.util.Scanner;
 
 public class PasswordChecker {
-    Scanner scanner = new Scanner(System.in);
+    
     //TO-DO
     static void main() {
         // User input for username and password
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your username:");
+        String username = scanner.nextLine();
+        System.out.println("Please enter your password:");
+        String password = scanner.nextLine();
         // Validate username and password
+        
         // re-prompt user if there is an invalid input
+        if (username.isEmpty() || password.isEmpty()) {
+            System.out.println("Invalid input. Please try again.");
+            main();
+        }
         // Calculate score
+    
         // Output password strength
         try {
-            Password password = new Password("hour", "1");
+            Password passwordObj = new Password("hour", "1");
         } catch (InvalidPasswordException e) {
             System.out.println(e.getMessage());;
         }
--- main.java
+++ main.java
@@ -1,21 +1,89 @@
+
+package logic;
+package model;
 import java.util.Scanner;
-public class main {
-    public static void main(String[] args) {
-        //Fei and Lika
-        //Input and output recall
-        //I want to add smth here to test the commit and push function of git
-        
-        
-        
-        //Hour and Yuth
-        //Password Validation
- 
-        
 
 
-        //Lix and miy
-        //Points calculator and output
-      
-       
+public class PasswordChecker {
+    Scanner scanner = new Scanner(System.in);
+    //TO-DO
+    static void main() {
+        // User input for username and password
+        // Validate username and password
+        // re-prompt user if there is an invalid input
+        // Calculate score
+        // Output password strength
+        try {
+            Password password = new Password("hour", "1");
+        } catch (InvalidPasswordException e) {
+            System.out.println(e.getMessage());;
         }
+
     }
+
+    public String classifyPassword(int score) {
+
+        return "im here";
+    }
+
+}
+
+public class Password {
+    private String username;
+    private String password;
+
+    public Password(String username, String password) throws InvalidPasswordException {
+
+        this.username = username;
+        // Validation logic here and throw exception.InvalidPasswordException if invalid
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Password{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
+
+public class PasswordScore {
+    Password password;
+
+    // TO DO
+    public static int calculateScore() {
+        // Return score for pattern and unique and more
+        return 1;
+    }
+
+    // TO DO
+    private int patternScore() {
+        return 1;
+    }
+
+    // TO DO
+    private int uniqueScore() {
+        return 1;
+    }
+
+    // TO DO: more functions
+}
+

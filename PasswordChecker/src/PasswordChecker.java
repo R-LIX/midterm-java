3ad8daa70127d5f733b93c5a50007b41d46b628c
R-LIX <davisrankwillix@gmail.com>
2026-03-08 08:16:46 +0700

    intro done

--- PasswordChecker/src/PasswordChecker.java
+++ PasswordChecker/src/PasswordChecker.java
@@ -9,12 +9,19 @@ public class PasswordChecker {
     static void main() {
         // User input for username and password
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your username:");
+        System.out.print("Please enter your username:");
         String username = scanner.nextLine();
-        System.out.println("Please enter your password:");
+        System.out.print("Please enter your password:");
         String password = scanner.nextLine();
         // Validate username and password
-        
+        System.out.print("Confirm set username & password:");
+        String confirm = scanner.nextLine();
+        if (confirm.equals("yes")){
+        System.out.print("username & password Confirm (yes/no): ");
+        }else{
+            System.out.println("Please input again.");
+            main();
+        }
         // re-prompt user if there is an invalid input
         if (username.isEmpty() || password.isEmpty()) {
             System.out.println("Invalid input. Please try again.");
@@ -23,13 +30,23 @@ public class PasswordChecker {
         // Calculate score
     
         // Output password strength
+        
         try {
             Password passwordObj = new Password("hour", "1");
         } catch (InvalidPasswordException e) {
-            System.out.println(e.getMessage());;
+            System.out.println(e.getMessage());
+        }
+        if(password.length() < 8){
+            System.out.println("Password must be at least 8 characters long.");
+            main();
+        }
+        if (password.contains(username)){
+            System.out.println("Password cannot contain the username!");
+            main();
         }
 
-    }
+}
+    
 
     public String classifyPassword(int score) {
 

12d077a643f4d7d3c62bfb1586792b7fa58679d8
DalikaLin <lindalika.kh@gmail.com>
2026-03-08 19:48:56 +0700

    Implement username and password validation
    Added username and password validation with scoring system.

--- main.java
+++ main.java
@@ -1,6 +1,61 @@
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String username;
+        String password;
+
+        while (true) {
+            System.out.print("Enter username: ");
+            username = input.nextLine().toLowerCase();
+
+            System.out.print("Enter password: ");
+            password = input.nextLine().toLowerCase();
+
+            if (containsUsernamePart(username, password)) {
+                System.out.println("Password must not contain parts of the username. Try again\n");
+            }
+            else {
+                break;
+            }
+        }
+
+        int score = 0;
+        if (password.length() >=8) {
+            score +=10;
+        }
+        if (password.matches(".*[!@#$%^&*(),.?\":{}|<>/\\\\\\[\\]_+=~`;-].*")) {
+            score +=10;
+        }
+        if (!password.equals(password.toLowerCase())) {
+            score +=10;
+        }
+
+        if (score < 15) {
+            System.out.println("Your password is weak.");
+        } else if (score == 15) {
+            System.out.println("Your password is medium.");
+        } else if (score > 15) {
+            System.out.println("Your password is strong.");
+        }
+    }
+
+    public static boolean containsUsernamePart(String username, String password) {
+
+        for (int i = 0; i < username.length(); i++) {
+
+            for (int j = i + 2; j <= username.length(); j++) {
+
+                String part = username.substring(i, j);
+
+                if (password.contains(part)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
         //Fei and Lika
         //Input and output recall
         //I want to add smth here to test the commit and push function of git

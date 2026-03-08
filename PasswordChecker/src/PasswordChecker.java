5692eb1a3b195c6d8a504e87e896d8fd3a02b117
Lymiy Chrean <lymiychrean@Lymiys-MacBook-Pro.local>
2026-03-08 11:54:09 +0700

    added calculate score (num, special char, capital letter) and classify password (weak, medium, strong) git push

--- PasswordChecker/src/PasswordChecker.java
+++ PasswordChecker/src/PasswordChecker.java
@@ -3,10 +3,11 @@ import model.Password;
 import logic.PasswordScore;
 import java.util.Scanner;
 
+
 public class PasswordChecker {
     
     //TO-DO
-    static void main() {
+    public static void main(String[] args) {
         // User input for username and password
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter your username:");
@@ -20,37 +21,46 @@ public class PasswordChecker {
         System.out.print("username & password Confirm (yes/no): ");
         }else{
             System.out.println("Please input again.");
-            main();
+            main(args);
         }
         // re-prompt user if there is an invalid input
         if (username.isEmpty() || password.isEmpty()) {
             System.out.println("Invalid input. Please try again.");
-            main();
+            main(args);
         }
         // Calculate score
     
         // Output password strength
-        
+        Password passwordObj = null;
         try {
-            Password passwordObj = new Password("hour", "1");
+            passwordObj = new Password(username, password);
         } catch (InvalidPasswordException e) {
             System.out.println(e.getMessage());
         }
         if(password.length() < 8){
             System.out.println("Password must be at least 8 characters long.");
-            main();
+            main(args);
         }
         if (password.contains(username)){
             System.out.println("Password cannot contain the username!");
-            main();
+            main(args);
         }
+        int score = PasswordScore.calculateScore(passwordObj);
+        String strength = classifyPassword(score);
+        System.out.println("Password Score: " + score);
+        System.out.println("Password Strength: " + strength);
 
 }
     
 
-    public String classifyPassword(int score) {
-
-        return "im here";
+    public static String classifyPassword(int score) {
+        if (score <= 33) {
+            return "Weak";
+        } else if (score <= 66) {
+            return "Medium";
+        } else {
+            return "Strong";
+        }
     }
 
 }
--- PasswordChecker/src/logic/PasswordScore.java
+++ PasswordChecker/src/logic/PasswordScore.java
@@ -6,9 +6,44 @@ public class PasswordScore {
     Password password;
 
     // TO DO
-    public static int calculateScore() {
+    public PasswordScore(Password password) {
+        this.password = password;
+    }
+
+    public static int calculateScore(Password password) {
         // Return score for pattern and unique and more
-        return 1;
+        int score = 0;
+        String pw = password.getPassword();
+
+        score += specialSymbolScore(pw);
+        score += numberScore(pw);
+        score += capitalScore(pw);
+
+        return score;
+    }
+
+    // +20 if password contains a special symbol
+    private static int specialSymbolScore(String pw) {
+        if (pw.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?`~].*")) {
+            return 20;
+        }
+        return 0;
+    }
+
+    // +20 if password contains a number
+    private static int numberScore(String pw) {
+        if (pw.matches(".*[0-9].*")) {
+            return 20;
+        }
+        return 0;
+    }
+
+    // +20 if password contains a capitalized letter
+    private static int capitalScore(String pw) {
+        if (pw.matches(".*[A-Z].*")) {
+            return 20;
+        }
+        return 0;
     }
 
     // TO DO

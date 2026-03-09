24e8226956638cda633e48a4d3a9f01c2ff601bb
QuirkyBoyZin <hoursebastian@gmail.com>
2026-03-09 23:42:07 +0700

    Implement inputPassword method to handle user input and process Password

--- PasswordChecker/src/PasswordChecker.java
+++ PasswordChecker/src/PasswordChecker.java
@@ -5,72 +5,92 @@ import java.util.Scanner;
 
 
 public class PasswordChecker {
-    
+    private final Scanner scanner = new Scanner(System.in);
+
     //TO-DO
-    public static void main(String[] args) {
+    public void main(String[] args) {
+
         // User input for username and password
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter your username:");
-        String username = scanner.nextLine();
-        System.out.print("Please enter your password:");
-        String password = scanner.nextLine();
-        // Validate username and password
-        System.out.print("Confirm set username & password (yes/no):");
-        String confirm = scanner.nextLine();
-        if (confirm.equals("yes")){
-        System.out.println("username & password Confirm! ");
-        }else{
-            System.out.println("Please input again.");
-            main(args);
+
+        Password password = inputPassword();
+        if (password == null) {
+            System.out.println("Goodbye");
+            return;
         }
-        // re-prompt user if there is an invalid input
-        if (username.isEmpty() || password.isEmpty()) {
-            System.out.println("Invalid input. Please try again.");
-            main(args);
-        }
-        // Calculate score
-    
-        // Output password strength
-        Password passwordObj = null;
-        try {
-            passwordObj = new Password(username, password);
-        } catch (InvalidPasswordException e) {
-            System.out.println(e.getMessage());
-        }
-        if(password.length() < 8){
-            System.out.println("Password must be at least 8 characters long.");
-            main(args);
-        }
-        if (password.contains(username)){
-            System.out.println("Password cannot contain the username!");
-            main(args);
-        }
-        int score = PasswordScore.calculateScore(passwordObj);
-        String strength = classifyPassword(score);
-        if(score > 0) {
-            System.out.println("Password Score: " + score);
-        
-        System.out.println("Password Strength: " + strength);
-        System.out.println("Want to try again? (yes/no):");
-        }
-        if (scanner.nextLine().equals("yes")){
-            main(args);
-    }else{
-            System.out.println("Goodbye!");
-        }
+
+//        // Calculate score
+//
+//        // Output password strength
+
+
+//
+//        int score = PasswordScore.calculateScore(passwordObj);
+//        String strength = classifyPassword(score);
+//
+//        if (score > 0) {
+//            System.out.println("Password Score: " + score);
+//
+//            System.out.println("Password Strength: " + strength);
+//            System.out.println("Want to try again? (yes/no):");
+//        }
+//
+//        if (scanner.nextLine().equals("yes")) {
+//            main(args);
+//        } else {
+//            System.out.println("Goodbye!");
+//        }
+//    }
+//
+//
+//
+
+    }
+//    public static String classifyPassword(int score) {
+//        if (score <= 33) {
+//            return "Weak";
+//        } else if (score <= 66 && score > 33) {
+//            return "Medium";
+//        } else {
+//            return "Strong";
+//        }
+    public Password inputPassword() {
+        while (true) {
+            System.out.print("Please enter your username: ");
+            String username = scanner.nextLine();
+
+            System.out.print("Please enter your password: ");
+            String password = scanner.nextLine();
+
+            // Validate username and password
+            System.out.printf("""
+                         
+                         Press 1 to confirm
+                         Press 2 to start over
+                         Press any key to abort
+                         ------------------------------------
+                         Username: %s
+                         Password: %s
+                         ------------------------------------
+                         """ , username, password);
+            String userInput= scanner.nextLine();
+            if (userInput.equals("1")) {
+                try {
+                    return new Password(username, password);
+                } catch (InvalidPasswordException ipe) {
+                    System.out.println(ipe.getMessage());
+                }
+
+            } else if (userInput.equals("2")) {
+                return inputPassword();
+
+            } else {
+                return null;
+            }
+
         }
 
 
-    
-
-    public static String classifyPassword(int score) {
-        if (score <= 33) {
-            return "Weak";
-        } else if (score <= 66 && score>33) {
-            return "Medium";
-        } else {
-            return "Strong";
-        }
     }
 
 }
+

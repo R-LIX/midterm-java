083eca6bfd75daaa16cf4dc0e000d3dd8ed5ea42
QuirkyBoyZin <hoursebastian@gmail.com>
2026-04-29 18:53:36 +0700

    First successful test run of FormService and UserRepository implementations

--- src/main/java/com/form/FormApplication.java
+++ src/main/java/com/form/FormApplication.java
@@ -1,4 +1,21 @@
 package com.form;
 
+import com.form.User.User;
+
+import java.util.Scanner;
+
 public class FormApplication {
+    private final FormService formService = new FormService();
+    private final Scanner  scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new FormApplication().run();
+    }
+
+    public void run() {
+
+        formService.signUp(new User("123", "123"));
+
+    }
+
 }
--- src/main/java/com/form/FormService.java
+++ src/main/java/com/form/FormService.java
@@ -1,4 +1,18 @@
 package com.form;
 
+import com.form.User.User;
+import com.form.User.UserRepository;
+
 public class FormService {
+    private final UserRepository userRepository = new UserRepository();
+
+    public boolean signIn(User user) {
+        return userRepository.exists(user);
+    }
+
+    public void signUp(User user) {
+        userRepository.saveUser(user);
+    }
+
+
 }
